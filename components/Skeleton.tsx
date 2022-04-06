20b596d51638e0e9eec9806c3573ec88b246d22f
Isaac Fidler <isaac.fidler@bx-earth.com>
2022-04-06 13:29:52 +0100

    feat: imeplements skeleton compo

--- components/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = { show: boolean };
-
-const Skeleton = ({ show }: Props) => {
-  return show && <></>;
-};
-
-export default Skeleton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Skeleton from "react-loading-skeleton";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Link
-        prefetch={true}
-        href={{
-          pathname: "/[username]",
-          query: { username: "isaacf147" },
-        }}
-      />
+      <Skeleton count={5} style={{ width: 50 }} />
     </div>
   );
 };
