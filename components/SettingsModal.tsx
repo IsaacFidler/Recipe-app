12179877a44413ba8f8c62430348dff665a94ccf
Isaac Fidler <isaac.fidler@bx-earth.com>
2022-04-12 17:23:48 +0100

    feat: download button

--- components/SettingsModal.tsx
+++ components/SettingsModal.tsx
@@ -1,36 +1,54 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import styles from "../styles/SettingsModal.module.scss";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
 import { AiOutlineClose } from "react-icons/ai";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { auth } from "../lib/firebase";
+import { auth, firestore } from "../lib/firebase";
+import toast from "react-hot-toast";
+import { doc, writeBatch } from "firebase/firestore";
+
 type Props = {
   user: any;
   isOpen: any;
   openModal: any;
-  closeModal: any;
+  settingOpen: any;
 };
 
-const SettingsModal = ({ user, isOpen, openModal, closeModal }: Props) => {
-  console.log(isOpen);
+const SettingsModal = ({ user, isOpen, openModal, settingOpen }: Props) => {
+  const [uploading, setUploading] = useState(false);
+  const [downloadURL, setDownloadURl] = useState<any>(null);
+
   const { register, handleSubmit } = useForm();
   const uid = auth.currentUser!.uid;
-  const onSubmit = (data: any) => {
+  const onSubmit = async (data: any) => {
+    setUploading(true);
     // Create a root reference
     const storage = getStorage();
 
     const file = data.picture[0];
     const fileName = file.name;
     // Create a reference to 'mountains.jpg'
-    const storageRef = ref(storage, `${uid}/profilePicture/${fileName}`);
+    const storageRef = ref(storage, `${uid}/${fileName}`);
     // 'file' comes from the Blob or File API
 
     // console.log(file);
-    uploadBytes(storageRef, file).then((snapshot) => {
-      console.log("Uploaded a blob or file!");
+    await uploadBytes(storageRef, file).then((snapshot) => {
+      toast("File uploaded!");
     });
+
+    setUploading(true);
+
+    getDownloadURL(ref(storage, `${uid}/${fileName}`))
+      .then((url) => {
+        setDownloadURl(url);
+      })
+      .catch((error) => {
+        // Handle any errors
+      });
+
+    settingOpen(false);
   };
 
   console.log(user.uid);
@@ -40,13 +58,18 @@ const SettingsModal = ({ user, isOpen, openModal, closeModal }: Props) => {
       style={{ display: isOpen ? "flex" : "none" }}
     >
       <h1>Settings</h1>
-      <button onClick={closeModal} className={styles.closeButton}>
-        <AiOutlineClose />
-      </button>
+
       <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register("picture")} type="file" />
         <button className="button1">Submit</button>
       </form>
+      <h1 style={{ display: uploading ? "flex" : "none" }}>Loading .....</h1>
+      {downloadURL && (
+        <code className="upload-snippet">{`![alt](${downloadURL})`}</code>
+      )}
+      <button onClick={() => settingOpen(false)} className="button1">
+        <AiOutlineClose className={styles.closeButton} />
+      </button>
     </div>
   );
 };
--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -7,23 +7,42 @@ type Props = { user: any };
 
 export default function UserProfile({ user }: Props) {
   const [settingsOpen, setSettingsOpen] = useState(false);
-  const closeModal = () => {
-    setSettingsOpen(false);
-  };
+
   return (
-    <div className="userProfileContainer">
-      <img src={user.photoURL || "/hacker.png"} className="card-img-center" />
-      <p>
-        <i>@{user.username}</i>
-      </p>
-      <h1>{user.displayName || "Anonymous User"}</h1>
-      <FiSettings onClick={() => setSettingsOpen(true)} />
+    <>
       <SettingModal
         user={user}
         isOpen={settingsOpen}
         openModal={undefined}
-        closeModal={closeModal}
+        settingOpen={setSettingsOpen}
       />
-    </div>
+      <div
+        style={{ filter: settingsOpen ? "blur(8px)" : "blur(0px)" }}
+        className="userProfileContainer"
+      >
+        <div></div>
+        <img src={user.photoURL || "/hacker.png"} className="card-img-center" />
+        <p>
+          <i>@{user.username}</i>
+        </p>
+        <h1>{user.displayName || "Anonymous User"}</h1>
+        <FiSettings
+          className={styles.settingsButton}
+          onClick={() => setSettingsOpen(true)}
+        />
+      </div>
+    </>
   );
+}
+
+{
+  /* <h1 style={{ display: uploading ? "flex" : "none" }}>Loading .....</h1>
+      {downloadURL && (
+        <code className="upload-snippet">{`![alt](${downloadURL})`}</code>
+      )} */
+}
+{
+  /* <button onClick={() => settingOpen(false)} className="button1">
+        <AiOutlineClose className={styles.closeButton} />
+      </button> */
 }
